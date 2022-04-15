e8885096bcd553097aa9f7f07ccf488f878cf3c0
Mariam Nersisyan <mariamnersisyan97@gmail.com>
2022-04-15 17:59:13 -0400

    api, fetch, & events added

--- src/index.js
+++ src/index.js
@@ -1,32 +1,47 @@
-const baseURL = `https://musicbrainz.org/ws/2/artist/?query={artistname}&fmt=json`;
+const baseURL = `https://musicbrainz.org/ws/2/artist/?query=${artist}&fmt=json`;
 const menu = document.getElementById("playlist");
 const musicButton = document.getElementById("music-button");
 const likeBtn = document.getElementById("add-to-playlist-button");
 
-
 window.addEventListener("DOMContentLoaded", () => {
    buttonEvent();
    playlistEvent();
-   addToPlaylistEvent();
+   getMusic();
+//    addToPlaylistEvent();
 });
 
+
+function getMusic() {
+    const ul = document.getElementById("artists-ul");
+    fetchMusic()
+    .then(data => { 
+        data.forEach(artist => {
+            ul.innerHTML +=   
+            `<li>${artist.name}</li>` 
+        })
+    })
+};
+
+async function fetchMusic() {
+    let res = await fetch(baseURL);
+    let data = await res.json();
+    return data;
+}
+
+function renderArtist(artist) {
+    const artistCard = 
+    `<div class ="card">
+    <h2> ${artist.name}<h2/>
+    <p>${artist.disambiguation}</p>
+    <p> Genre: ${artist} </p>
+    `
+    const card = document.getElementsByClassName("artist-card");
+    card.innerHTML += artistCard;
+}
 function buttonEvent () {
     musicButton.addEventListener("click", () => {
     getMusic();
 })
-};
-
-function getMusic() {
-    fetch(baseURL)
-    .then((res) => 
-    {
-        return res.json();
-    })
-    .then((data) =>
-    { 
-        console.log(data.disambiguation);
-        console.log(data.artists);
-    })
 };
 
 function playlistEvent () {
@@ -35,12 +50,15 @@ function playlistEvent () {
   })
 };
 
-function addToPlaylistEvent () {
-    likeBtn.addEventListener("click", () =>{
-        console.log("yay")
-    })
-}
+// function addToPlaylistEvent () {
+//     likeBtn.addEventListener("click", () =>{
+
+//     })}
+
+
+
 // .split("") then .join()
+//  + ${artist}.split(" ").join("_")
 // the names of the artists 
 // error handle of artist fetch 
 // submit listener for artist search 
