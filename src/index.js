8b7a2e6719282424e05459e85321d283346f1124
Mariam Nersisyan <mariamnersisyan97@gmail.com>
2022-04-19 18:58:58 -0400

    get data, render data in card

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-// const baseURL = `"https://musicbrainz.org/ws/2/artist/?query=" + ${artist} + "&fmt=json"`;
-// const artist = 
-const baseURL = "https://musicbrainz.org/ws/2/artist/?query=Madonna&fmt=json";
+const artist = document.getElementById("text-input").value;
+const baseURL = `https://musicbrainz.org/ws/2/artist/?query=${artist}&fmt=json`;
 const menu = document.getElementById("playlist");
 const musicButton = document.getElementById("music-button");
 const likeBtn = document.getElementById("add-to-playlist-button");
-const inputInit = input = document.getElementById("text-input");
-var input = document.getElementById("text-input").value;
+const inputInit = document.getElementById("text-input");
+
 
 window.addEventListener("DOMContentLoaded", () => {
    buttonEvent();
@@ -16,7 +15,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
 
 function getMusic() {
-    const ul = document.getElementById("artists-ul");
+    // const ul = document.getElementById("artist-ul");
     fetchMusic()
     .then(data => { 
         // data.map(artist => {
@@ -31,23 +30,28 @@ async function fetchMusic() {
     let res = await fetch(baseURL);
     let data = await res.json();
     return data;
-}
+};
 
-function renderArtist(artist) {
-    const artistCard = 
-    `<div class ="card">
-    <h2> ${artist.name}<h2/>
-    <p>${artist.disambiguation}</p>
-    <p> Genre: ${artist} </p>
-    `
-    const card = document.getElementsByClassName("artist-card");
-    card.innerHTML += artistCard;
-}
 function buttonEvent () {
     musicButton.addEventListener("click", () => {
     getMusic();
 })
 };
+
+function renderArtist(artist) {
+    const cardTitle = document.getElementById("card-title");
+    const cardText = document.getElementById("card-text");
+    cardTitle = artist.name
+    cardText = artist.disambiguation
+    // const artistCard = 
+    // `<div class ="card">
+    // <h2> ${artist.name}<h2/>
+    // <p>${artist.disambiguation}</p>
+    // <p> Genre: ${artist} </p>
+    // `
+    // const card = document.getElementsByClassName("artist-card");
+    // card.innerHTML += artistCard;
+}
 
 function playlistEvent () {
   menu.addEventListener("click", () => {
